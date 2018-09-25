e6d5dcd1941db892339681632757530b9b3331e4
chriseth <chris@ethereum.org>
2018-09-25 16:29:46 +0200

    Test case that shows that break/unbreak are more or less inverse.

--- test/libyul/YulOptimizerTest.cpp
+++ test/libyul/YulOptimizerTest.cpp
@@ -110,6 +110,20 @@ bool YulOptimizerTest::run(ostream& _stream, string const& _linePrefix, bool con
 		NameDispenser nameDispenser(*m_ast);
 		ExpressionSplitter{nameDispenser}(*m_ast);
 	}
+	else if (m_optimizerStep == "expressionJoiner")
+	{
+		disambiguate();
+		ExpressionJoiner::run(*m_ast);
+	}
+	else if (m_optimizerStep == "splitJoin")
+	{
+		disambiguate();
+		NameDispenser nameDispenser;
+		nameDispenser.m_usedNames = NameCollector(*m_ast).names();
+		ExpressionSplitter{nameDispenser}(*m_ast);
+		ExpressionJoiner::run(*m_ast);
+		ExpressionJoiner::run(*m_ast);
+	}
 	else if (m_optimizerStep == "functionGrouper")
 	{
 		disambiguate();
@@ -166,11 +180,6 @@ bool YulOptimizerTest::run(ostream& _stream, string const& _linePrefix, bool con
 	{
 		disambiguate();
 		UnusedPruner::runUntilStabilised(*m_ast);
-	}
-	else if (m_optimizerStep == "expressionJoiner")
-	{
-		disambiguate();
-		ExpressionJoiner::run(*m_ast);\
 	}
 	else if (m_optimizerStep == "ssaTransform")
 	{
