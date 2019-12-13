263c2b7d4ac1ffbe46edf30407a7ebc693987c14
Shulinkang <ShulinKang@tron.network>
2019-12-13 15:18:56 +0800

    change path to correct one

--- scripts/travis-emscripten/build_emscripten.sh
+++ scripts/travis-emscripten/build_emscripten.sh
@@ -48,7 +48,7 @@ if ! type wget &>/dev/null; then
     apt-get -y install wget
 fi
 
-WORKSPACE=/root/solidity
+WORKSPACE=/root/project
 
 # Increase nodejs stack size
 if ! [ -e /emsdk_portable/node/bin/node_orig ]
