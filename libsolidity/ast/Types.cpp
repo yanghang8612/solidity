5e70e6bea019b2e3be412dbeae8e669e075ac8d5
Timothy <57668523+timothyckw@users.noreply.github.com>
2020-08-04 11:41:24 +0800

    set unfreeze to lowercase

--- libsolidity/ast/Types.cpp
+++ libsolidity/ast/Types.cpp
@@ -481,7 +481,7 @@ MemberList::MemberMap AddressType::nativeMembers(ContractDefinition const*) cons
 		members.emplace_back(MemberList::Member{"transfer", TypeProvider::function(strings{"uint"}, strings(), FunctionType::Kind::Transfer, false, StateMutability::NonPayable)});
 		members.emplace_back(MemberList::Member{"transferToken", TypeProvider::function(strings{"uint", "trcToken"}, strings(), FunctionType::Kind::TransferToken)});
         members.emplace_back(MemberList::Member{"freeze", TypeProvider::function(strings{"uint", "uint", "uint"}, strings{"bool"}, FunctionType::Kind::Freeze, false, StateMutability::NonPayable)});
-        members.emplace_back(MemberList::Member{"unFreeze", TypeProvider::function(strings{"uint"}, strings{"bool"}, FunctionType::Kind::Unfreeze, false, StateMutability::NonPayable)});
+        members.emplace_back(MemberList::Member{"unfreeze", TypeProvider::function(strings{"uint"}, strings{"bool"}, FunctionType::Kind::Unfreeze, false, StateMutability::NonPayable)});
         members.emplace_back(MemberList::Member{"withdrawReward", TypeProvider::function(strings{}, strings{"bool"}, FunctionType::Kind::WithdrawReward, false, StateMutability::NonPayable)});
 	}
 	return members;
