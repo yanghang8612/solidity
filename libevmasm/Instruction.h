d72451b662c8afbed4e86d2ac40815f27dcaa229
Timothy <57668523+timothyckw@users.noreply.github.com>
2020-08-05 18:55:06 +0800

    remove iswitness

--- libevmasm/Instruction.h
+++ libevmasm/Instruction.h
@@ -206,9 +206,6 @@ enum class Instruction: uint8_t
 //	NATIVEFREEZE,
 //	NATIVEUNFREEZE,
 //  NATIVEVOTE,
-	
-	
-    ISWITNESS,
 
 	CREATE = 0xf0,		///< create a new account with associated code
 	CALL,				///< message-call into an account
